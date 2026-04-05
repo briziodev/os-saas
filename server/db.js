d76e25e16d3df1fe3cc1faefe94f1c1fd81700ee
Fabricio Bazana <https://github.com/briziodev/html.git>
2026-04-05 13:07:34 -0300

    debug: log render database config

--- server/db.js
+++ server/db.js
@@ -1,15 +1,23 @@
-
-
-
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const isProduction = process.env.DATABASE_URL;
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      }
+);
 
 module.exports = pool;
