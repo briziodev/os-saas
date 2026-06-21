7408fb2280e29bb4e47286855e18b4833addf3a3
fabricio bazana <fabriciopsse@gmail.com>
2026-06-21 13:04:00 -0300

    feat: padroniza icones do AppShell com Iconify

---
+++ web/src/components/AppIcon.jsx
@@ -0,0 +1,16 @@
+﻿import { Icon } from "@iconify/react";
+
+export function AppIcon({ icon, size = 20, className = "", title, ...props }) {
+  return (
+    <Icon
+      icon={icon}
+      width={size}
+      height={size}
+      className={className}
+      aria-hidden={title ? undefined : "true"}
+      aria-label={title}
+      role={title ? "img" : undefined}
+      {...props}
+    />
+  );
+}
